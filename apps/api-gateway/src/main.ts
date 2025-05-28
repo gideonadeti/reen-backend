8d89826814b974aceb2aa5e3b378d478d47c82a8
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-28 12:14:24 +0000

    feat(main): Add Swagger documentation setup for API Gateway
    Integrate Swagger for API documentation in the API Gateway module.
    This enhances the developer experience by providing a user-friendly
    interface for exploring the API endpoints.

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+
 import { ApiGatewayModule } from './api-gateway.module';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create(ApiGatewayModule);
+  const config = new DocumentBuilder()
+    .setTitle('API Gateway')
+    .setDescription('API Gateway for REEN backend')
+    .setVersion('1.0.0')
+    .build();
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (_controllerKey: string, methodKey: string) =>
+      methodKey,
+  };
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, options);
+
+  SwaggerModule.setup('/api-gateway/documentation', app, documentFactory);
+
   await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
+};
+
+void bootstrap();
