6ff235bf8f5f23a2083b409ea95b9f3432ef7a84
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-31 22:17:16 +0000

    feat(auth): Set up GRPC microservice app

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
-import { AuthModule } from './auth.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-  await app.listen(process.env.port ?? 3000);
-}
+import { AuthModule } from './auth.module';
+import { AUTH_PACKAGE_NAME } from '@app/protos/generated/auth';
+
+const bootstrap = async () => {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: AUTH_PACKAGE_NAME,
+        protoPath: join(__dirname, '../../libs/protos/auth.proto'),
+      },
+    },
+  );
+
+  await app.listen();
+};
+
 bootstrap();
