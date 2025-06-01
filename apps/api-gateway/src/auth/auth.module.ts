07655561e7d36abf148bc31093634200bde8293a
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-01 06:12:00 +0000

    feat(auth): Integrate gRPC client for authentication service
    Set up the gRPC client in AuthService for communication with the
    authentication service. This includes the necessary imports and
    initialization logic for the client.

--- apps/api-gateway/src/auth/auth.module.ts
+++ apps/api-gateway/src/auth/auth.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { AUTH_PACKAGE_NAME } from '@app/protos';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: AUTH_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          package: AUTH_PACKAGE_NAME,
+          protoPath: join(__dirname, '../../libs/protos/auth.proto'),
+        },
+      },
+    ]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- apps/api-gateway/src/auth/auth.service.ts
+++ apps/api-gateway/src/auth/auth.service.ts
@@ -1,10 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { Response } from 'express';
+import { ClientGrpc } from '@nestjs/microservices';
 
 import { SignUpDto } from './dtos/sign-up.dto';
+import {
+  AUTH_PACKAGE_NAME,
+  AUTH_SERVICE_NAME,
+  AuthServiceClient,
+} from '@app/protos';
 
 @Injectable()
-export class AuthService {
+export class AuthService implements OnModuleInit {
+  constructor(@Inject(AUTH_PACKAGE_NAME) private authClient: ClientGrpc) {}
+
+  private authService: AuthServiceClient;
+
+  onModuleInit() {
+    this.authService =
+      this.authClient.getService<AuthServiceClient>(AUTH_SERVICE_NAME);
+  }
+
   signUp(signUpDto: SignUpDto, res: Response) {
     res.json(signUpDto);
   }
