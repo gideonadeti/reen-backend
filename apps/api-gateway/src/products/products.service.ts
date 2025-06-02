2693fcee355b1bdce53171a2d43c301eb756ae9a
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-02 22:32:19 +0000

    feat(products): Implement gRPC client for product creation
    Add gRPC client integration to handle product creation requests.
    Implement error handling and logging for better debugging.

--- apps/api-gateway/src/products/products.service.ts
+++ apps/api-gateway/src/products/products.service.ts
@@ -1,12 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { GrpcError } from '@app/interfaces';
+import {
+  PRODUCTS_PACKAGE_NAME,
+  PRODUCTS_SERVICE_NAME,
+  ProductsServiceClient,
+} from '@app/protos/generated/products';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @Inject(PRODUCTS_PACKAGE_NAME) private productsClient: ClientGrpc,
+  ) {}
+
+  private productsService: ProductsServiceClient;
+  private logger = new Logger(ProductsService.name);
+
+  onModuleInit() {
+    this.productsService = this.productsClient.getService(
+      PRODUCTS_SERVICE_NAME,
+    );
+  }
+
+  private handleError(error: any, action: string) {
+    this.logger.error(`Failed to ${action}`, (error as GrpcError).stack);
+
+    throw new InternalServerErrorException(`Failed to ${action}`);
+  }
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      return await firstValueFrom(
+        this.productsService.create(createProductDto),
+      );
+    } catch (error) {
+      this.handleError(error as GrpcError, 'create product');
+    }
   }
 
   findAll() {
