072c59fa99b6d1249c31730c5c9186c297e1f3df
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-02 22:36:34 +0000

    feat(products): Register gRPC client in products module

--- apps/api-gateway/src/products/products.module.ts
+++ apps/api-gateway/src/products/products.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { PRODUCTS_PACKAGE_NAME } from '@app/protos/generated/products';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: PRODUCTS_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          package: PRODUCTS_PACKAGE_NAME,
+          protoPath: join(__dirname, '../../libs/protos/products.proto'),
+        },
+      },
+    ]),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
