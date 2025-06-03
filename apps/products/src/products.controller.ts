0b04b3581004411f2680adee6303243cd3e6f6cc
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-03 06:08:02 +0000

    feat(products): Implement gRPC create method for products

--- apps/products/src/products.controller.ts
+++ apps/products/src/products.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+
 import { ProductsService } from './products.service';
+import {
+  CreateRequest,
+  PRODUCTS_SERVICE_NAME,
+} from '@app/protos/generated/products';
 
 @Controller()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Get()
-  getHello(): string {
-    return this.productsService.getHello();
+  @GrpcMethod(PRODUCTS_SERVICE_NAME)
+  create(data: CreateRequest) {
+    return this.productsService.create(data);
   }
 }
--- apps/products/src/products.service.ts
+++ apps/products/src/products.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+
+import { PrismaService } from './prisma/prisma.service';
+import { CreateRequest } from '@app/protos/generated/products';
 
 @Injectable()
 export class ProductsService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prismaService: PrismaService) {}
+
+  private logger = new Logger(ProductsService.name);
+
+  private handleError(error: any, action: string) {
+    this.logger.error(`Failed to ${action}`, (error as Error).stack);
+
+    throw new RpcException(JSON.stringify(error));
+  }
+
+  async create({ adminId, ...createProductDto }: CreateRequest) {
+    try {
+      return await this.prismaService.product.create({
+        data: { ...createProductDto, adminId },
+      });
+    } catch (error) {
+      this.handleError(error, 'create product');
+    }
   }
 }
