8d8053b6f6bd18fbc64014bc75470592e86f77e7
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-04 12:44:33 +0000

    feat(cart-items): Add ConfigModule for environment configuration

--- apps/cart-items/src/cart-items.module.ts
+++ apps/cart-items/src/cart-items.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { CartItemsController } from './cart-items.controller';
 import { CartItemsService } from './cart-items.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'apps/cart-items/.env',
+    }),
+  ],
   controllers: [CartItemsController],
   providers: [CartItemsService],
 })
