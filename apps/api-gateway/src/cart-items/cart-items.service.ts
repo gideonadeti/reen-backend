26a1d1df3ed4bb2d824f4297843c67d76dd56e27
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-04 12:27:59 +0000

    feat(cart-items): Implement GRPC client and error handling
    Add GRPC client initialization and error handling in CartItemsService.

--- apps/api-gateway/src/cart-items/cart-items.service.ts
+++ apps/api-gateway/src/cart-items/cart-items.service.ts
@@ -1,9 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+
 import { CreateCartItemDto } from './dto/create-cart-item.dto';
 import { UpdateCartItemDto } from './dto/update-cart-item.dto';
+import {
+  CART_ITEMS_PACKAGE_NAME,
+  CART_ITEMS_SERVICE_NAME,
+  CartItemsServiceClient,
+} from '@app/protos/generated/cart-items';
+import { GrpcError } from '@app/interfaces';
 
 @Injectable()
 export class CartItemsService {
+  constructor(
+    @Inject(CART_ITEMS_PACKAGE_NAME) private cartItemsClient: ClientGrpc,
+  ) {}
+
+  private cartItemsService: CartItemsServiceClient;
+  private logger = new Logger(CartItemsService.name);
+
+  onModuleInit() {
+    this.cartItemsService = this.cartItemsClient.getService(
+      CART_ITEMS_SERVICE_NAME,
+    );
+  }
+
+  private handleError(error: any, action: string) {
+    this.logger.error(`Failed to ${action}`, (error as GrpcError).stack);
+
+    throw new InternalServerErrorException(`Failed to ${action}`);
+  }
+
   create(createCartItemDto: CreateCartItemDto) {
     return 'This action adds a new cartItem';
   }
