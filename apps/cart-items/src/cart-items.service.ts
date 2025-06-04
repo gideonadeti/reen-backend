3fa13924cffbd17e31c3a6476b1b2b5629d40e82
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-04 12:52:30 +0000

    feat(cart-items): Add error handling and logging to service

--- apps/cart-items/src/cart-items.service.ts
+++ apps/cart-items/src/cart-items.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class CartItemsService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prismaService: PrismaService) {}
+
+  private logger = new Logger(CartItemsService.name);
+
+  private handleError(error: any, action: string) {
+    this.logger.error(`Failed to ${action}`, (error as Error).stack);
+
+    throw new RpcException(JSON.stringify(error));
   }
 }
