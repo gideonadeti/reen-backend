385d71a0023b11be19d90e135017938ee0594c12
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-04 14:02:35 +0000

    feat(cart-items): Integrate gRPC client for product service

--- apps/cart-items/src/cart-items.module.ts
+++ apps/cart-items/src/cart-items.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 import { CartItemsController } from './cart-items.controller';
 import { CartItemsService } from './cart-items.service';
 import { PrismaService } from './prisma/prisma.service';
 import { GrpcLoggingInterceptor } from '@app/interceptors';
+import { PRODUCTS_PACKAGE_NAME } from '@app/protos/generated/products';
 
 @Module({
   imports: [
@@ -13,6 +16,17 @@ import { GrpcLoggingInterceptor } from '@app/interceptors';
       isGlobal: true,
       envFilePath: 'apps/cart-items/.env',
     }),
+    ClientsModule.register([
+      {
+        name: PRODUCTS_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          package: PRODUCTS_PACKAGE_NAME,
+          protoPath: join(__dirname, '../../libs/protos/products.proto'),
+          url: 'localhost:5002',
+        },
+      },
+    ]),
   ],
   controllers: [CartItemsController],
   providers: [
--- apps/cart-items/src/cart-items.service.ts
+++ apps/cart-items/src/cart-items.service.ts
@@ -1,13 +1,28 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { RpcException } from '@nestjs/microservices';
+import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc, RpcException } from '@nestjs/microservices';
 
 import { PrismaService } from './prisma/prisma.service';
+import {
+  PRODUCTS_PACKAGE_NAME,
+  ProductsServiceClient,
+} from '@app/protos/generated/products';
+import { CART_ITEMS_SERVICE_NAME } from '@app/protos/generated/cart-items';
 
 @Injectable()
-export class CartItemsService {
-  constructor(private prismaService: PrismaService) {}
+export class CartItemsService implements OnModuleInit {
+  constructor(
+    private prismaService: PrismaService,
+    @Inject(PRODUCTS_PACKAGE_NAME) private productsClient: ClientGrpc,
+  ) {}
 
   private logger = new Logger(CartItemsService.name);
+  private productsService: ProductsServiceClient;
+
+  onModuleInit() {
+    this.productsService = this.productsClient.getService(
+      CART_ITEMS_SERVICE_NAME,
+    );
+  }
 
   private handleError(error: any, action: string) {
     this.logger.error(`Failed to ${action}`, (error as Error).stack);
