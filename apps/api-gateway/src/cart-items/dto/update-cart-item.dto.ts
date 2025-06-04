22d7e80b12f9ed62cdb15861837cb5ef815cda9b
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-04 19:50:02 +0000

    refactor(cart-items): Remove PartialType from UpdateCartItemDto

--- apps/api-gateway/src/cart-items/dto/update-cart-item.dto.ts
+++ apps/api-gateway/src/cart-items/dto/update-cart-item.dto.ts
@@ -1,4 +1,3 @@
-import { PartialType } from '@nestjs/swagger';
 import { CreateCartItemDto } from './create-cart-item.dto';
 
-export class UpdateCartItemDto extends PartialType(CreateCartItemDto) {}
+export class UpdateCartItemDto extends CreateCartItemDto {}
