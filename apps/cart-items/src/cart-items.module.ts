07a798ca28720d4594e210302c750a67c9a634a0
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-04 12:49:13 +0000

    feat(cart-items): Add GrpcLoggingInterceptor to providers

--- apps/cart-items/src/cart-items.module.ts
+++ apps/cart-items/src/cart-items.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 import { CartItemsController } from './cart-items.controller';
 import { CartItemsService } from './cart-items.service';
 import { PrismaService } from './prisma/prisma.service';
+import { GrpcLoggingInterceptor } from '@app/interceptors';
 
 @Module({
   imports: [
@@ -13,6 +15,13 @@ import { PrismaService } from './prisma/prisma.service';
     }),
   ],
   controllers: [CartItemsController],
-  providers: [CartItemsService, PrismaService],
+  providers: [
+    CartItemsService,
+    PrismaService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: GrpcLoggingInterceptor,
+    },
+  ],
 })
 export class CartItemsModule {}
