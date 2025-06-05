49122a99ea4c7ab00a707339c5403192193ba748
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-05 13:07:06 +0000

    feat(events-handler): Inject Orders gRPC client and service

--- apps/events-handler/src/events-handler.service.ts
+++ apps/events-handler/src/events-handler.service.ts
@@ -11,16 +11,23 @@ import {
   PRODUCTS_SERVICE_NAME,
   ProductsServiceClient,
 } from '@app/protos/generated/products';
+import {
+  ORDERS_PACKAGE_NAME,
+  ORDERS_SERVICE_NAME,
+  OrdersServiceClient,
+} from '@app/protos/generated/orders';
 
 @Injectable()
 export class EventsHandlerService implements OnModuleInit {
   constructor(
     @Inject(CART_ITEMS_PACKAGE_NAME) private cartItemsClient: ClientGrpc,
     @Inject(PRODUCTS_PACKAGE_NAME) private productsClient: ClientGrpc,
+    @Inject(ORDERS_PACKAGE_NAME) private ordersClient: ClientGrpc,
   ) {}
 
   private cartItemsService: CartItemsServiceClient;
   private productsService: ProductsServiceClient;
+  private ordersService: OrdersServiceClient;
 
   onModuleInit() {
     this.cartItemsService = this.cartItemsClient.getService(
@@ -29,5 +36,6 @@ export class EventsHandlerService implements OnModuleInit {
     this.productsService = this.productsClient.getService(
       PRODUCTS_SERVICE_NAME,
     );
+    this.ordersService = this.ordersClient.getService(ORDERS_SERVICE_NAME);
   }
 }
