c65d9170e0823fe679e4ad8188566cfac38927a7
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-05 11:41:30 +0000

    feat(events-handler): Integrate ConfigModule for environment setup
    Added ConfigModule to the EventsHandlerModule for global
    configuration management using environment variables. This
    enables better handling of configuration settings across
    the microservice.

--- apps/events-handler/src/events-handler.module.ts
+++ apps/events-handler/src/events-handler.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { EventsHandlerController } from './events-handler.controller';
 import { EventsHandlerService } from './events-handler.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'apps/events-handler/.env',
+    }),
+  ],
   controllers: [EventsHandlerController],
   providers: [EventsHandlerService],
 })
--- apps/events-handler/src/main.ts
+++ apps/events-handler/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { AsyncMicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+
 import { EventsHandlerModule } from './events-handler.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(EventsHandlerModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<AsyncMicroserviceOptions>(
+    EventsHandlerModule,
+    {
+      useFactory: (configService: ConfigService) => ({
+        transport: Transport.RMQ,
+        options: {
+          urls: [configService.get('MESSAGE_BROKER_URL') as string],
+          queue: 'events-handler',
+        },
+      }),
+      inject: [ConfigService],
+    },
+  );
+
+  await app.listen();
 }
-bootstrap();
+
+void bootstrap();
