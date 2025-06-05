7f0d16b1dda974cb207304ace5a1731ad0f01bf7
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-05 13:15:02 +0000

    refactor(orders): Remove unused getHello method from OrdersService

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,12 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+
 import { OrdersService } from './orders.service';
 
 @Controller()
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
-
-  @Get()
-  getHello(): string {
-    return this.ordersService.getHello();
-  }
 }
