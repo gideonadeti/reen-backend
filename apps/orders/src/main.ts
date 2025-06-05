8154ad4c0683cfc8f00b4afcfac8dbcad7b13013
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-05 12:45:35 +0000

    feat(orders): Implement gRPC microservice setup for Orders

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { OrdersModule } from './orders.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
-async function bootstrap() {
-  const app = await NestFactory.create(OrdersModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
+import { OrdersModule } from './orders.module';
+import { ORDERS_PACKAGE_NAME } from '@app/protos/generated/orders';
+
+const bootstrap = async () => {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    OrdersModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: ORDERS_PACKAGE_NAME,
+        protoPath: join(__dirname, '../../libs/protos/orders.proto'),
+        url: 'localhost:5003',
+      },
+    },
+  );
+
+  await app.listen();
+};
+
+void bootstrap();
