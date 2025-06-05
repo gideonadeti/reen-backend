70aad76b0f682e08829b9fc80536b4b187c74bb1
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-05 12:25:30 +0000

    refactor(events-handler): Remove unused getHello method

--- apps/events-handler/src/events-handler.controller.ts
+++ apps/events-handler/src/events-handler.controller.ts
@@ -1,12 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { EventsHandlerService } from './events-handler.service';
 
 @Controller()
 export class EventsHandlerController {
   constructor(private readonly eventsHandlerService: EventsHandlerService) {}
-
-  @Get()
-  getHello(): string {
-    return this.eventsHandlerService.getHello();
-  }
 }
--- apps/events-handler/src/events-handler.service.ts
+++ apps/events-handler/src/events-handler.service.ts
@@ -1,8 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class EventsHandlerService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
+export class EventsHandlerService {}
