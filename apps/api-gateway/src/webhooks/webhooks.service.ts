f71768e80de69f33801f133f7ae5eac9dbb6ea3d
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-05 08:22:54 +0000

    feat(webhooks): Implement payment service integration
    Add constructor and onModuleInit method to WebhooksService
    for initializing the payment service client using GRPC.

--- apps/api-gateway/src/webhooks/webhooks.service.ts
+++ apps/api-gateway/src/webhooks/webhooks.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+
+import {
+  PAYMENT_PACKAGE_NAME,
+  PAYMENT_SERVICE_NAME,
+  PaymentServiceClient,
+} from '@app/protos/generated/payment';
 
 @Injectable()
-export class WebhooksService {}
+export class WebhooksService {
+  constructor(
+    @Inject(PAYMENT_PACKAGE_NAME) private paymentClient: ClientGrpc,
+  ) {}
+
+  private paymentService: PaymentServiceClient;
+
+  onModuleInit() {
+    this.paymentService = this.paymentClient.getService(PAYMENT_SERVICE_NAME);
+  }
+}
