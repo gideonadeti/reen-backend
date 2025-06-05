d9e2578a07c4d8fc4a9f9b1e7f7552efd5f72fcb
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-05 13:23:52 +0000

    feat(orders): Implement create method in OrdersService
    Add create method to handle order creation with error logging.
    Enhance OrdersController to utilize the new service method.

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,8 +1,18 @@
 import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
 
 import { OrdersService } from './orders.service';
+import {
+  CreateRequest,
+  ORDERS_SERVICE_NAME,
+} from '@app/protos/generated/orders';
 
 @Controller()
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
+
+  @GrpcMethod(ORDERS_SERVICE_NAME)
+  create(data: CreateRequest) {
+    return this.ordersService.create(data);
+  }
 }
--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+
+import { CreateRequest } from '@app/protos/generated/orders';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  private logger = new Logger(OrdersService.name);
+
+  private handleError(error: any, action: string) {
+    this.logger.error(`Failed to ${action}`, (error as Error).stack);
+
+    throw new RpcException(JSON.stringify(error));
+  }
+
+  async create({ userId, total, orderItems }: CreateRequest) {
+    try {
+      return await this.prismaService.order.create({
+        data: {
+          userId,
+          total,
+          orderItems: {
+            createMany: {
+              data: orderItems,
+            },
+          },
+        },
+        include: {
+          orderItems: true,
+        },
+      });
+    } catch (error) {
+      this.handleError(error, 'create order');
+    }
+  }
+}
