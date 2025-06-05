50ec9c6769920cf6df6187e9196f4f003403b1b1
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-05 12:20:48 +0000

    feat(webhooks): Implement Stripe webhook handling for checkout session
    Add handling for Stripe checkout session completed events in the
    WebhooksService. This includes constructing the event from the raw
    body and emitting the event to the events handler service.

--- apps/api-gateway/src/webhooks/webhooks.controller.ts
+++ apps/api-gateway/src/webhooks/webhooks.controller.ts
@@ -1,8 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Headers, Post, RawBodyRequest, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 import { WebhooksService } from './webhooks.service';
 
 @Controller('webhooks')
 export class WebhooksController {
   constructor(private readonly webhooksService: WebhooksService) {}
+
+  @Post('stripe')
+  handleCheckoutSessionCompleted(
+    @Req() req: RawBodyRequest<Request>,
+    @Headers('stripe-signature') sig: string,
+  ) {
+    return this.webhooksService.handleCheckoutSessionCompleted(req, sig);
+  }
 }
--- apps/api-gateway/src/webhooks/webhooks.service.ts
+++ apps/api-gateway/src/webhooks/webhooks.service.ts
@@ -1,9 +1,57 @@
-import { Inject, Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
+import { Request } from 'express';
+import { ConfigService } from '@nestjs/config';
 import { ClientProxy } from '@nestjs/microservices';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  RawBodyRequest,
+} from '@nestjs/common';
 
 @Injectable()
 export class WebhooksService {
   constructor(
     @Inject('EVENTS_HANDLER_SERVICE') private eventsHandlerClient: ClientProxy,
+    private readonly configService: ConfigService,
   ) {}
+
+  private logger = new Logger(WebhooksService.name);
+
+  private handleError(error: any, action: string) {
+    this.logger.error(`Failed to ${action}`, (error as Error).stack);
+
+    throw new InternalServerErrorException(`Failed to ${action}`);
+  }
+
+  handleCheckoutSessionCompleted(req: RawBodyRequest<Request>, sig: string) {
+    const webhookSecret = this.configService.get(
+      'STRIPE_WEBHOOK_SIGNING_SECRET',
+    ) as string;
+    const stripeSecretKey = this.configService.get(
+      'STRIPE_SECRET_KEY',
+    ) as string;
+    const stripe = new Stripe(stripeSecretKey);
+
+    try {
+      const event = stripe.webhooks.constructEvent(
+        req.rawBody as Buffer,
+        sig,
+        webhookSecret,
+      );
+
+      this.logger.log(`Stripe event received: ${event.type}`);
+
+      if (event.type === 'checkout.session.completed') {
+        const session = event.data.object;
+
+        this.eventsHandlerClient.emit('checkout-session-completed', session);
+      }
+
+      return { received: true };
+    } catch (error) {
+      this.handleError(error, 'handle checkout session completed');
+    }
+  }
 }
