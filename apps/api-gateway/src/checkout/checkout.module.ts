ae01f9fc1383f6ed26430cfcea83ed3608b02ac1
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-05 10:40:37 +0000

    feat(checkout): Integrate GRPC clients for cart and products services
    Added GRPC client registrations for cart items and products services in
    the Checkout module. This enables communication with the respective
    microservices for handling checkout operations.

--- apps/api-gateway/src/checkout/checkout.module.ts
+++ apps/api-gateway/src/checkout/checkout.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 import { CheckoutService } from './checkout.service';
 import { CheckoutController } from './checkout.controller';
+import { CART_ITEMS_PACKAGE_NAME } from '@app/protos/generated/cart-items';
+import { PRODUCTS_PACKAGE_NAME } from '@app/protos/generated/products';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: CART_ITEMS_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          package: CART_ITEMS_PACKAGE_NAME,
+          protoPath: join(__dirname, '../../libs/protos/cart-items.proto'),
+          url: 'localhost:5002',
+        },
+      },
+    ]),
+    ClientsModule.register([
+      {
+        name: PRODUCTS_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          package: PRODUCTS_PACKAGE_NAME,
+          protoPath: join(__dirname, '../../libs/protos/products.proto'),
+          url: 'localhost:5001',
+        },
+      },
+    ]),
+  ],
   controllers: [CheckoutController],
   providers: [CheckoutService],
 })
--- apps/api-gateway/src/checkout/checkout.service.ts
+++ apps/api-gateway/src/checkout/checkout.service.ts
@@ -1,15 +1,44 @@
 import {
   BadRequestException,
+  Inject,
   Injectable,
   InternalServerErrorException,
   Logger,
+  OnModuleInit,
 } from '@nestjs/common';
 
 import { GrpcError, MicroserviceError } from '@app/interfaces';
+import {
+  CART_ITEMS_PACKAGE_NAME,
+  CART_ITEMS_SERVICE_NAME,
+  CartItemsServiceClient,
+} from '@app/protos/generated/cart-items';
+import { ClientGrpc } from '@nestjs/microservices';
+import {
+  PRODUCTS_PACKAGE_NAME,
+  PRODUCTS_SERVICE_NAME,
+  ProductsServiceClient,
+} from '@app/protos/generated/products';
 
 @Injectable()
-export class CheckoutService {
+export class CheckoutService implements OnModuleInit {
+  constructor(
+    @Inject(CART_ITEMS_PACKAGE_NAME) private cartItemsClient: ClientGrpc,
+    @Inject(PRODUCTS_PACKAGE_NAME) private productsClient: ClientGrpc,
+  ) {}
+
   private logger = new Logger(CheckoutService.name);
+  private cartItemsService: CartItemsServiceClient;
+  private productsService: ProductsServiceClient;
+
+  onModuleInit() {
+    this.cartItemsService = this.cartItemsClient.getService(
+      CART_ITEMS_SERVICE_NAME,
+    );
+    this.productsService = this.productsClient.getService(
+      PRODUCTS_SERVICE_NAME,
+    );
+  }
 
   private handleError(error: any, action: string) {
     this.logger.error(`Failed to ${action}`, (error as GrpcError).stack);
