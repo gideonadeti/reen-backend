c7f1a370982641323bfcf573fadd1e3b786dfb2d
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-05 06:20:23 +0000

    feat(payment): Integrate GRPC clients for products and cart items

--- apps/payment/src/payment.service.ts
+++ apps/payment/src/payment.service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+
+import {
+  CART_ITEMS_PACKAGE_NAME,
+  CART_ITEMS_SERVICE_NAME,
+  CartItemsServiceClient,
+} from '@app/protos/generated/cart-items';
+import {
+  PRODUCTS_PACKAGE_NAME,
+  PRODUCTS_SERVICE_NAME,
+  ProductsServiceClient,
+} from '@app/protos/generated/products';
 
 @Injectable()
-export class PaymentService {
-  getHello(): string {
-    return 'Hello World!';
+export class PaymentService implements OnModuleInit {
+  constructor(
+    @Inject(PRODUCTS_PACKAGE_NAME) private productsClient: ClientGrpc,
+    @Inject(CART_ITEMS_PACKAGE_NAME) private cartItemsClient: ClientGrpc,
+  ) {}
+
+  private productsService: ProductsServiceClient;
+  private cartItemsService: CartItemsServiceClient;
+
+  onModuleInit() {
+    this.productsService = this.productsClient.getService(
+      PRODUCTS_SERVICE_NAME,
+    );
+    this.cartItemsService = this.cartItemsClient.getService(
+      CART_ITEMS_SERVICE_NAME,
+    );
   }
 }
