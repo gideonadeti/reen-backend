894fae3ec36b591db663b4c5044436681eb4777a
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-05 08:18:58 +0000

    feat(webhooks): Register GRPC client for payment service

--- apps/api-gateway/src/webhooks/webhooks.module.ts
+++ apps/api-gateway/src/webhooks/webhooks.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 import { WebhooksService } from './webhooks.service';
 import { WebhooksController } from './webhooks.controller';
+import { PAYMENT_PACKAGE_NAME } from '@app/protos/generated/payment';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: PAYMENT_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          package: PAYMENT_PACKAGE_NAME,
+          protoPath: join(__dirname, '../../libs/protos/payment.proto'),
+          url: 'localhost:5003',
+        },
+      },
+    ]),
+  ],
   controllers: [WebhooksController],
   providers: [WebhooksService],
 })
