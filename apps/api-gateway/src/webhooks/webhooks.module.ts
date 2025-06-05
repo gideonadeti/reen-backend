ab53ab2a9f2a5ececb211f35fab8786fba0e6914
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-05 11:49:39 +0000

    feat(webhooks): Integrate events handler service with RMQ

--- apps/api-gateway/src/webhooks/webhooks.module.ts
+++ apps/api-gateway/src/webhooks/webhooks.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { WebhooksService } from './webhooks.service';
 import { WebhooksController } from './webhooks.controller';
 
 @Module({
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        imports: [ConfigModule],
+        name: 'EVENTS_HANDLER_SERVICE',
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get('MESSAGE_BROKER_URL') as string],
+            queue: 'events-handler',
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
+  ],
   controllers: [WebhooksController],
   providers: [WebhooksService],
 })
--- apps/api-gateway/src/webhooks/webhooks.service.ts
+++ apps/api-gateway/src/webhooks/webhooks.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
-export class WebhooksService {}
+export class WebhooksService {
+  constructor(
+    @Inject('EVENTS_HANDLER_SERVICE') private eventsHandlerClient: ClientProxy,
+  ) {}
+}
