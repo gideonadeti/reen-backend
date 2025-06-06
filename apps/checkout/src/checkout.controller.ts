8c63a13f29dffd99b37e8e2458a7c2746033dd92
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-06 08:26:46 +0000

    feat(checkout): Implement checkout process with Stripe integration
    Add a new checkout method in CheckoutService that handles the
    checkout process using Stripe. This includes fetching cart items,
    validating the cart, and creating a Stripe checkout session.
    Also, replace the previous getHello method with a gRPC method
    for creating a checkout session.

--- apps/checkout/src/checkout.controller.ts
+++ apps/checkout/src/checkout.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+
 import { CheckoutService } from './checkout.service';
+import {
+  CHECKOUT_SERVICE_NAME,
+  CheckoutRequest,
+} from '@app/protos/generated/checkout';
 
 @Controller()
 export class CheckoutController {
   constructor(private readonly checkoutService: CheckoutService) {}
 
-  @Get()
-  getHello(): string {
-    return this.checkoutService.getHello();
+  @GrpcMethod(CHECKOUT_SERVICE_NAME)
+  create(data: CheckoutRequest) {
+    return this.checkoutService.checkout(data.userId);
   }
 }
--- apps/checkout/src/checkout.service.ts
+++ apps/checkout/src/checkout.service.ts
@@ -1,7 +1,15 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { ClientGrpc } from '@nestjs/microservices';
+import Stripe from 'stripe';
+import { ClientGrpc, RpcException } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
+import { firstValueFrom } from 'rxjs';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
 
+import { GrpcError, MicroserviceError } from '@app/interfaces';
 import {
   CART_ITEMS_PACKAGE_NAME,
   CART_ITEMS_SERVICE_NAME,
@@ -23,6 +31,7 @@ export class CheckoutService {
 
   private cartItemsService: CartItemsServiceClient;
   private productsService: ProductsServiceClient;
+  private logger = new Logger(CheckoutService.name);
 
   onModuleInit() {
     this.cartItemsService = this.cartItemsClient.getService(
@@ -33,7 +42,73 @@ export class CheckoutService {
     );
   }
 
-  getHello(): string {
-    return 'Hello World!';
+  private handleError(error: any, action: string) {
+    this.logger.error(`Failed to ${action}`, (error as Error).stack);
+
+    // Check if error is a gRPC error and throw it else api-gateway wouldn't parse correctly
+    const microserviceError = JSON.parse(
+      (error as GrpcError).details || '{}',
+    ) as MicroserviceError;
+
+    if (Object.keys(microserviceError).length !== 0) {
+      throw new RpcException(JSON.stringify(microserviceError));
+    }
+
+    throw new RpcException(JSON.stringify(error));
+  }
+
+  async checkout(userId: string) {
+    try {
+      const findAllResponse = await firstValueFrom(
+        this.cartItemsService.findAll({ userId }),
+      );
+      const cartItems = findAllResponse.cartItems || [];
+
+      if (cartItems.length === 0) {
+        throw new BadRequestException(
+          'Cart is empty. Cannot proceed to checkout.',
+        );
+      }
+
+      const productIds = cartItems.map((cartItem) => cartItem.productId);
+      const findByIdsResponse = await firstValueFrom(
+        this.productsService.findByIds({
+          ids: productIds,
+        }),
+      );
+      const products = findByIdsResponse.products || [];
+      const productMap = new Map(products.map((p) => [p.id, p]));
+      const frontendBaseUrl = this.configService.get(
+        'FRONTEND_BASE_URL',
+      ) as string;
+      const stripeSecretKey = this.configService.get(
+        'STRIPE_SECRET_KEY',
+      ) as string;
+      const stripe = new Stripe(stripeSecretKey);
+      const session = await stripe.checkout.sessions.create({
+        mode: 'payment',
+        line_items: cartItems.map((item) => ({
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: productMap.get(item.productId)!.name,
+            },
+            unit_amount: Number(productMap.get(item.productId)?.price) * 100,
+          },
+          quantity: item.quantity,
+        })),
+        metadata: {
+          userId,
+        },
+        success_url: `${frontendBaseUrl}/checkout?success=true`,
+        cancel_url: `${frontendBaseUrl}/checkout?canceled=true`,
+      });
+
+      return {
+        stripeSessionUrl: session.url,
+      };
+    } catch (error) {
+      this.handleError(error, 'checkout');
+    }
   }
 }
