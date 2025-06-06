abe02c6ec5f480f46e18cbb7e9f30b220d6b24c4
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-06 07:10:01 +0000

    feat(orders): Implement gRPC client in OrdersService
    Add gRPC client integration to OrdersService for communication
    with the orders microservice. This includes the necessary
    imports and initialization logic for the service.

--- apps/api-gateway/src/orders/orders.module.ts
+++ apps/api-gateway/src/orders/orders.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { join } from 'path';
 
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ORDERS_PACKAGE_NAME } from '@app/protos/generated/orders';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: ORDERS_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          package: ORDERS_PACKAGE_NAME,
+          protoPath: join(__dirname, '../../libs/protos/orders.proto'),
+          url: 'localhost:5003',
+        },
+      },
+    ]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
--- apps/api-gateway/src/orders/orders.service.ts
+++ apps/api-gateway/src/orders/orders.service.ts
@@ -1,7 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+
+import {
+  ORDERS_PACKAGE_NAME,
+  ORDERS_SERVICE_NAME,
+  OrdersServiceClient,
+} from '@app/protos/generated/orders';
 
 @Injectable()
-export class OrdersService {
+export class OrdersService implements OnModuleInit {
+  constructor(
+    @Inject(ORDERS_PACKAGE_NAME) private productsClient: ClientGrpc,
+  ) {}
+
+  private ordersService: OrdersServiceClient;
+
+  onModuleInit() {
+    this.ordersService = this.productsClient.getService(ORDERS_SERVICE_NAME);
+  }
+
   findAll() {
     return `This action returns all orders`;
   }
