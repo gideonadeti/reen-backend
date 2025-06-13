1b91249f88f4d5c10b8534ad6e8b059f7ba406e5
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-13 15:02:51 +0000

    feat(auth): Implement functionality to attach user to request when they're authenticated via clerk auth guard

--- apps/api-gateway/src/auth/guards/clerk-auth.guard.ts
+++ apps/api-gateway/src/auth/guards/clerk-auth.guard.ts
@@ -1,13 +1,70 @@
-import { getAuth } from '@clerk/express';
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
 import { Request } from 'express';
+import { firstValueFrom } from 'rxjs';
+import { clerkClient, getAuth } from '@clerk/express';
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+
+import {
+  AUTH_PACKAGE_NAME,
+  AUTH_SERVICE_NAME,
+  AuthServiceClient,
+  User,
+} from '@app/protos/generated/auth';
 
 @Injectable()
-export class ClerkAuthGuard implements CanActivate {
-  canActivate(context: ExecutionContext) {
+export class ClerkAuthGuard implements CanActivate, OnModuleInit {
+  constructor(@Inject(AUTH_PACKAGE_NAME) private authClient: ClientGrpc) {}
+
+  private authService: AuthServiceClient;
+  private logger = new Logger(ClerkAuthGuard.name);
+
+  onModuleInit() {
+    this.authService = this.authClient.getService(AUTH_SERVICE_NAME);
+  }
+
+  async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest<Request>();
     const { userId } = getAuth(request);
 
+    if (userId !== null) {
+      try {
+        await this.attachUserToRequest(userId, request);
+      } catch (error) {
+        this.logger.error('Failed to attach user:', (error as Error).stack);
+
+        return false;
+      }
+    }
+
     return userId !== null;
+  }
+
+  async attachUserToRequest(clerkId: string, req: Request) {
+    let user = await firstValueFrom(
+      this.authService.findUserByClerkId({ clerkId }),
+    );
+
+    if (!user) {
+      const clerkUser = await clerkClient.users.getUser(clerkId);
+
+      const signUpResponse = await firstValueFrom(
+        this.authService.signUp({
+          name: clerkUser.fullName as string,
+          email: clerkUser.primaryEmailAddress!.emailAddress,
+          clerkId,
+        }),
+      );
+
+      user = signUpResponse.user as User;
+    }
+
+    req['user'] = user;
   }
 }
