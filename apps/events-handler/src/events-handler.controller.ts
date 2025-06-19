4f9f3dc35c5a8502540ceaddc60cf4279e9695b7
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-19 13:59:30 +0000

    refactor(events): Remove unused event handlers for notifications

--- apps/events-handler/src/events-handler.controller.ts
+++ apps/events-handler/src/events-handler.controller.ts
@@ -3,7 +3,6 @@ import { Controller } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
 
 import { EventsHandlerService } from './events-handler.service';
-import { AdminNotificationPayload } from '@app/interfaces/admin-notification-payload/admin-notification-payload.interface';
 
 @Controller()
 export class EventsHandlerController {
@@ -12,16 +11,6 @@ export class EventsHandlerController {
   @EventPattern('checkout-session-completed')
   handleCheckoutSessionCompleted(@Payload() data: Stripe.Checkout.Session) {
     return this.eventsHandlerService.handleCheckoutSessionCompleted(data);
-  }
-
-  @EventPattern('send-order-confirmation')
-  handleSendOrderConfirmation(@Payload() data: string) {
-    return this.eventsHandlerService.handleSendOrderConfirmation(data);
-  }
-
-  @EventPattern('send-admin-notifications')
-  handleSendAdminNotifications(@Payload() data: AdminNotificationPayload[]) {
-    return this.eventsHandlerService.handleSendAdminNotifications(data);
   }
 
   @EventPattern('update-quantities')
