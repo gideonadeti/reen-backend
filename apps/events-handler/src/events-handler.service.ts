c6ba843ae2fff7a632a7024e6e40bf94ebe6bf80
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-28 01:59:46 +0000

    refactor(events): Remove apply financial effects requests

--- apps/events-handler/src/events-handler.service.ts
+++ apps/events-handler/src/events-handler.service.ts
@@ -147,15 +147,6 @@ export class EventsHandlerService
         idempotencyKey: uuidv4(),
       }));
 
-      const applyFinancialEffectsRequests = Array.from(
-        adminOrderItemsMap.entries(),
-      ).map(([adminId, orderItems]) => ({
-        userId,
-        adminId,
-        amount: orderItems.reduce((acc, item) => acc + item.price, 0),
-        idempotencyKey: uuidv4(),
-      }));
-
       const adminNotificationPayloads = Array.from(
         adminOrderItemsMap.entries(),
       ).map(([adminId, orderItems]) => ({
@@ -170,7 +161,6 @@ export class EventsHandlerService
         total,
         orderItems,
         updateBalancesRequests,
-        applyFinancialEffectsRequests,
         adminNotificationPayloads,
       };
 
