3af3c5dfc3ee8449ee482927247dcc8d2d6f65c7
Gideon Adeti <gideonadeti0@gmail.com>
2025-06-30 03:59:07 +0000

    feat(mailersend): Implement MailersendService with configuration
    Add MailersendService to handle email sending functionality.
    Includes constructor setup for MailerSend API key and sender details.

--- apps/events-handler/src/mailersend/mailersend.service.ts
+++ apps/events-handler/src/mailersend/mailersend.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MailerSend, Sender } from 'mailersend';
 
 @Injectable()
-export class MailersendService {}
+export class MailersendService {
+  private mailersend: MailerSend;
+  private sentFrom: Sender;
+
+  constructor(private readonly configService: ConfigService) {
+    this.mailersend = new MailerSend({
+      apiKey: configService.get('MAILERSEND_API_KEY') as string,
+    });
+
+    this.sentFrom = new Sender(
+      'test-yxj6lj91dk14do2r.mlsender.net',
+      'Gideon Adeti',
+    );
+  }
+}
