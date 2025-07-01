7fd5948a5d78ae5fc6288b85454bd6f97bf00b06
Gideon Adeti <gideonadeti0@gmail.com>
2025-07-01 11:50:41 +0000

    feat(gen-protos): Update proto file handling and logging
    Refactor the command to generate protos by allowing specific proto files
    to be passed as arguments. Enhance success logging to indicate which
    proto file was generated or if all files were processed.

--- gen-protos.ts
+++ gen-protos.ts
@@ -6,20 +6,27 @@ const outputDir = `${inputDir}/generated`;
 
 mkdirSync(outputDir, { recursive: true });
 
+const [, , protoName] = process.argv;
+
+const protoFiles = protoName
+  ? `${inputDir}/${protoName}.proto`
+  : `${inputDir}/*.proto`;
+
 const command = [
   'protoc',
   `--ts_proto_out=${outputDir}`,
   '--ts_proto_opt=nestJs=true,useDate=true',
   `--proto_path=${inputDir}`,
-  `${inputDir}/*.proto`,
-  `&&`,
-  `nest build protos`,
+  protoFiles,
+  '&&',
+  'nest build protos',
 ].join(' ');
 
 try {
   execSync(command, { stdio: 'inherit' });
-
-  console.log('Protos generated successfully');
+  console.log(
+    `Protos generated successfully ${protoName ? `for ${protoName}.proto` : 'for all files'}`,
+  );
 } catch (error) {
   console.error('Failed to generate protos:', error);
   process.exit(1);
